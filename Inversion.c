2a269cbd6472ab97b69a10d486a1f973a1a987fe
atulyasndrm <aatulya21@iitk.ac.in>
2022-11-09 20:52:41 +0530

    Suo

---
+++ Inversion.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ms(int*a, int l1, int r1, int l2,int r2){
+    int* c=malloc()
+while(l1<=r1&&l2<=r2){
+    if(a[l1]>a[l2]){
+
+    }
+}
+
+
+}
+int invs(int*a, int l,int r){
+
+int count1=invs(a,l,(l+r)/2);
+int count2=invs(a,((l+r)/2)+1,r);
+
+
+}
+
+
+
+
+
+int main()
+{   int n; scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",a+i);
+    }
+    invs(a,0,n-1);
+
+    return 0;
+}
